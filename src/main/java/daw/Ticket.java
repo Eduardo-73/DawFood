0270a78bcc13f94c088f09418b35cfa4b9272e2a
Eduardo-73 <eduardocorreaaranda@gmail.com>
2024-01-23 12:50:14 +0100

    clase ticket

---
+++ src/main/java/daw/Ticket.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author edu
+ */
+public class Ticket {
+
+    private static Random sc = new Random();
+
+    private int id;
+    private int turno;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private double precioTotal;
+    private ArrayList<Productos> lista;
+    private static int contador = 1;
+
+    // Constructor
+    public Ticket(double precioTotal, ArrayList<Productos> lista) {
+        this.id = contador++;
+        this.turno = sc.nextInt(111, 999);
+        this.fecha = LocalDate.now();
+        this.hora = LocalTime.now();
+        this.precioTotal = precioTotal;
+        this.lista = lista;
+    }
+
+    // Get y Set
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public ArrayList<Productos> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Productos> lista) {
+        this.lista = lista;
+    }
+
+    // ToString 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String prod = "";
+        for (Productos productos : lista) {
+            prod += " " + productos.getStock() + " " + productos.getDescripcion()
+                    + " " + productos.getPrecio() + "\n";
+        }
+
+        String txt = "-------------------------------------------------------------------------------------\n"
+                + "                                     DawFood\n"
+                + "Turno: " + turno + "\n"
+                + "Fecha:\t" + fecha + " Hora: " + hora.getHour() + ":" + hora.getMinute() + "\n"
+                + "-------------------------------------------------------------------------------------"
+                + "\nCantidad     Producto     Precio\n"
+                + prod + "\n"
+                + "--------------------------------------------------------------------------------------\n"
+                + "Precio Total: " + precioTotal + "€" + "\n"
+                + "--------------------------------------------------------------------------------------\n";
+        sb.append(txt);
+        return sb.toString();
+    }
+
+}
